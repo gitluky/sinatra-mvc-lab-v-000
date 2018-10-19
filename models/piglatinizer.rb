e63d7d1ad8514dbfa8cb90e057cc9ef520d46c8a
Jonathan Luk <jluk87@gmail.com>
2018-10-19 20:49:48 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
   attr_accessor :user_input, :piglatinized_input
-  @@vowels = ["a","e","i","o","u","A", "E","I","O","U"]
+  @@vowels = ["a","e","i","o","u","A","E","I","O","U"]
 
   def initialize(user_input)
     @user_input = user_input
