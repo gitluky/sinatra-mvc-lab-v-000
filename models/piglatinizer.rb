f84501635866982e060a61fbc34f73ecdf872a28
Jonathan Luk <jluk87@gmail.com>
2018-10-19 20:48:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,20 @@
 class PigLatinizer
   attr_accessor :user_input, :piglatinized_input
+  @@vowels = ["a","e","i","o","u"]
 
   def initialize(user_input)
     @user_input = user_input
   end
 
+  def piglatinize
+
+    @user_input.split(" ")
+    @user_input.each do |word|
+      @starting_consonants = []
+      letter_array = word.split
+      letter_array.each do |letter|
+
+  end
 
 
 
